5b09e021c76bee6f7e1231f8f97ab5ca555606cf
Facundo Prieto <facundo.prieto321@gmail.com>
2025-12-10 01:27:49 -0300

    Implement link redirection and add destination retrieval logic

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,11 +1,25 @@
+import { getDestinationForCountry } from '@/helpers/route-ops';
 import { getLink } from '@repo/data-ops/queries/links';
+import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
 import { Hono } from 'hono';
 
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
 	const id = c.req.param('id');
-	const linkInfoFromDb = await getLink(id);
-	console.log(linkInfoFromDb);
-	return c.json(linkInfoFromDb);
+
+	const linkInfo = await getLink(id);
+	if (!linkInfo) {
+		return c.text("Destination not found", 404);
+	}
+
+	const cfHeaders = cloudflareInfoSchema.safeParse(c.req.raw.cf);
+	if (!cfHeaders.success) {
+		return c.text("Invalid Cloudflare headers", 500);
+	}
+
+	const headers = cfHeaders.data
+	const destination = getDestinationForCountry(linkInfo, headers.country);
+
+	return c.redirect(destination, 301);
 });
